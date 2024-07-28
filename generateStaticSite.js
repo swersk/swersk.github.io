4c14f97eeb9db72ab558908249d7acb99f26c244
Lauren Swersky <lswersky@greenpeace.org>
2024-07-27 21:30:28 -0400

    Initial commit of static WordPress site

---
+++ generateStaticSite.js
@@ -0,0 +1,90 @@
+const fs = require('fs-extra');
+const path = require('path');
+const http = require('http');
+const https = require('https');
+const { XMLParser } = require('fast-xml-parser');
+const ejs = require('ejs');
+
+// Read and parse the XML file
+const xmlFilePath = path.join(__dirname, 'missionnutrition.wordpress.2024-07-28.000.xml');
+const xmlData = fs.readFileSync(xmlFilePath, 'utf-8');
+const parser = new XMLParser();
+const jsonObj = parser.parse(xmlData);
+
+// Extract posts and attachments
+const posts = jsonObj.rss.channel.item;
+
+// Define paths
+const outputDir = path.join(__dirname, 'static-site');
+const templateDir = path.join(__dirname, 'templates');
+fs.ensureDirSync(outputDir);
+
+// Process each post and attachment
+(async function() {
+    let pinned_posts = [];
+    let post_list = [];
+
+    for (const post of posts) {
+        // Download attachments
+        if (post['wp:post_type'] === 'attachment') {
+            const url = post['wp:attachment_url'];
+            const protocol = url.startsWith('https') ? https : http;
+            for (const post_meta of post['wp:postmeta']) {
+                if (post_meta['wp:meta_key'] === '_wp_attached_file') {
+                    const file_path = post_meta['wp:meta_value'];
+                    const full_path = path.join(outputDir, 'wp-content/uploads', file_path);
+                    fs.ensureDirSync(path.dirname(full_path));
+                    const file = fs.createWriteStream(full_path);
+                    protocol.get(url, (resp) => {
+                        resp.pipe(file);
+                        file.on("finish", () => {
+                            file.close();
+                        });
+                    });
+                }
+            }
+        }
+
+        // Generate post page if it's published
+        if (post['wp:post_type'] === 'post' && post['pubDate']) {
+            post['content:encoded'] = post['content:encoded']
+                .split(/\r?\n|\r|\n/g)
+                .reduce((accumulator, currentValue) => accumulator + `<p>${currentValue}</p>`, '');
+
+            const content = await ejs.renderFile(path.join(templateDir, 'post.ejs'), { post: post });
+            const postDir = path.join(outputDir, 'archives', post['wp:post_id'].toString());
+            fs.ensureDirSync(postDir);
+            fs.writeFileSync(path.join(postDir, 'index.html'), content);
+
+            const element = {
+                id: post['wp:post_id'].toString(),
+                title: post.title,
+                summary: post['content:encoded'].replace(/<[^>]*>?/gm, '').slice(0, 300)
+            };
+
+            // Assuming 'pinned_post_ids' is an array of strings or numbers
+            if (['pinned_post_ids'].includes(post['wp:post_id'])) {
+                pinned_posts.push(element);
+            } else {
+                post_list.push(element);
+            }
+        }
+    }
+
+    // Generate table of contents (toc)
+    pinned_posts.sort((a, b) => b.id - a.id);
+    let merged_posts = pinned_posts.concat(post_list.sort((a, b) => b.id - a.id));
+
+    // Generate README.md
+    let readme = `# My WordPress Blog\nThis is a backup of my WordPress blog.\n\n`;
+    merged_posts.forEach(post => {
+        readme += `[${post.title}](archives/${post.id})\n\n`;
+    });
+    fs.writeFileSync(path.join(outputDir, 'README.md'), readme);
+
+    // Generate index.html (table of contents)
+    const tocContent = await ejs.renderFile(path.join(templateDir, 'toc.ejs'), { posts: merged_posts });
+    fs.writeFileSync(path.join(outputDir, 'index.html'), tocContent);
+
+    console.log('Static site generated successfully.');
+})();
---
+++ static-site/README.md
@@ -0,0 +1,45 @@
+# My WordPress Blog
+This is a backup of my WordPress blog.
+
+[Spanish Recipe](archives/290)
+
+[Overcoming Feelings of Powerlessness with Food](archives/266)
+
+[Can beer be healthy?](archives/210)
+
+[Why you Should Eat in Season](archives/192)
+
+[Which oil should you use?](archives/177)
+
+[Guaca-Olé!](archives/167)
+
+[It's all over by the time you're 5](archives/162)
+
+[Sugar Research is Exploding!](archives/147)
+
+[Small Changes go a Long Way](archives/140)
+
+[Yogurt: the good, the bad, and the ugly](archives/103)
+
+[Orange Juice: healthy or not so healthy?](archives/98)
+
+[Wooo!](archives/88)
+
+[What happens when you eat processed foods](archives/86)
+
+[Another food documentary](archives/63)
+
+[Why the Paleo Diet is BS](archives/60)
+
+[Carrots with Soy Sauce](archives/31)
+
+[Not all frozen foods are created equal](archives/26)
+
+[Sugar's bad. I get it. What about](archives/24)
+
+[Sugar: An intro](archives/22)
+
+[The truth behind Starbucks fraps](archives/18)
+
+[Welcome!](archives/2)
+
