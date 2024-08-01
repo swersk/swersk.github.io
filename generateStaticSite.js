3d78cec52661d44252daa5f119c77751a904621e
Lauren Swersky <lswersky@greenpeace.org>
2024-07-31 22:07:18 -0400

    refactor: generateStaticSite.js

--- generateStaticSite.js
+++ generateStaticSite.js
@@ -4,13 +4,7 @@ const http = require('http');
 const https = require('https');
 const { XMLParser } = require('fast-xml-parser');
 const ejs = require('ejs');
-
-// Function to decode HTML entities
-function decodeHtml(html) {
-    const txt = document.createElement('textarea');
-    txt.innerHTML = html;
-    return txt.value;
-}
+const he = require('he'); // Import the he library for HTML decoding
 
 // Read and parse the XML file
 const xmlFilePath = path.join(__dirname, 'laurenswersky.wordpress.2024-07-28.000.xml');
@@ -26,6 +20,23 @@ const outputDir = path.join(__dirname, 'static-site');
 const templateDir = path.join(__dirname, 'templates');
 fs.ensureDirSync(outputDir);
 
+// Function to download a file
+async function downloadFile(url, dest) {
+    const protocol = url.startsWith('https') ? https : http;
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(dest);
+        protocol.get(url, (response) => {
+            response.pipe(file);
+            file.on('finish', () => {
+                file.close(resolve);
+            });
+        }).on('error', (err) => {
+            fs.unlink(dest);
+            reject(err);
+        });
+    });
+}
+
 // Process each post and attachment
 (async function() {
     let pinned_posts = [];
@@ -35,20 +46,13 @@ fs.ensureDirSync(outputDir);
         // Download attachments
         if (post['wp:post_type'] === 'attachment') {
             const url = post['wp:attachment_url'];
-            const protocol = url.startsWith('https') ? https : http;
             if (Array.isArray(post['wp:postmeta'])) {
                 for (const post_meta of post['wp:postmeta']) {
                     if (post_meta['wp:meta_key'] === '_wp_attached_file') {
                         const file_path = post_meta['wp:meta_value'];
                         const full_path = path.join(outputDir, 'wp-content/uploads', file_path);
                         fs.ensureDirSync(path.dirname(full_path));
-                        const file = fs.createWriteStream(full_path);
-                        protocol.get(url, (resp) => {
-                            resp.pipe(file);
-                            file.on("finish", () => {
-                                file.close();
-                            });
-                        });
+                        await downloadFile(url, full_path);
                     }
                 }
             }
@@ -56,12 +60,9 @@ fs.ensureDirSync(outputDir);
 
         // Generate post page if it's published
         if (post['wp:post_type'] === 'post' && post['pubDate']) {
-            // Decode HTML entities
-            post['content:encoded'] = decodeHtml(post['content:encoded']);
-
-            post['content:encoded'] = post['content:encoded']
+            post['content:encoded'] = he.decode(post['content:encoded']
                 .split(/\r?\n|\r|\n/g)
-                .reduce((accumulator, currentValue) => accumulator + `${currentValue}`, '');
+                .reduce((accumulator, currentValue) => accumulator + `${currentValue}`, ''));
 
             const content = await ejs.renderFile(path.join(templateDir, 'post.ejs'), { post: post });
             const postDir = path.join(outputDir, 'archives', post['wp:post_id'].toString());
