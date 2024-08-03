3c9761bb65c5ed7bcccb37ff1a1c9938190ac86b
Lauren Swersky <lswersky@greenpeace.org>
2024-08-02 23:44:22 -0400

    deps: remove xml parser

--- generateStaticSite.js
+++ generateStaticSite.js
@@ -80,7 +80,7 @@ const mainIndexHtml = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Archive</title>
+    <title>Lauren Swersky</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
